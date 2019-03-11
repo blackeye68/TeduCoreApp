f3557b54c444f48d4bef16acea4a2ce9e7e2ad89
tu.cuong.bat.tuc@gmail.com <24867193@vohoi>
2019-03-12 00:37:04 +0700

    gfdsfghtrdfsfg

---
+++ TeduCoreApp.Data.EF/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TeduCoreApp.Data.Entities;
+
+namespace TeduCoreApp.Data.EF
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
