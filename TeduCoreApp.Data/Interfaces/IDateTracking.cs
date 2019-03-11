ff886b4c42e444ecb8c5bbe647a791616b963ee6
tu.cuong.bat.tuc@gmail.com <24867193@vohoi>
2019-03-11 18:09:53 +0700

    fasffssdsfsdadf

---
+++ TeduCoreApp.Data/Interfaces/IDateTracking.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TeduCoreApp.Data.Interfaces
+{
+    internal interface IDateTracking
+    {
+        DateTime DateCreated { get; set; }
+        DateTime DateModified { get; set; }
+    }
+}
---
+++ TeduCoreApp.Data/Interfaces/ISwithchable.cs
@@ -0,0 +1,9 @@
+﻿using TeduCoreApp.Data.Enums;
+
+namespace TeduCoreApp.Data.Interfaces
+{
+    public interface ISwithchable
+    {
+        Status Status { set; get; }
+    }
+}
